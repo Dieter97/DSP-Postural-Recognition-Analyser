5677dbd74b081153611b16a5c138fdc5e8944304
wartek69 <alextcher@hotmail.com>
2017-12-20 17:04:45 +0100

    Added window parameter slider

--- gui.m
+++ gui.m
@@ -22,7 +22,7 @@ function varargout = gui(varargin)
 
 % Edit the above text to modify the response to help gui
 
-% Last Modified by GUIDE v2.5 19-Dec-2017 22:20:54
+% Last Modified by GUIDE v2.5 20-Dec-2017 17:04:17
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -495,3 +495,25 @@ function filterPlot_Callback(hObject, eventdata, handles)
 
 % Hint: get(hObject,'Value') returns toggle state of filterPlot
 replotFrequency(handles);
+
+
+% --- Executes on slider movement.
+function window_edit_Callback(hObject, eventdata, handles)
+% hObject    handle to window_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+
+
+% --- Executes during object creation, after setting all properties.
+function window_edit_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to window_edit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
