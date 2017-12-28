810bd9ed17861c75cd701f7a623592baf1f58a88
Niels Balemans <balemans.niels@gmail.com>
2017-12-28 20:28:55 +0100

    fix

--- test.m
+++ test.m
@@ -1,30 +1,38 @@
 clear; clc;
 figure(1);
-f=20; %Hz
+f=25; %Hz
 fs=f*10; 
-t=-1:1/fs:1;
+
+% Read data from excel sheet
+x = transpose(xlsread('standing still eyes closed.xlsx', 'b12:b1400'));
+x = repmat(x,1,40);
+% Create time axis
+t = 0:1/fs:((1/fs)*(length(x)-1));
+
+%t = -1:1/fs:1;
+
 n = length(t);
 
 % function
 %x = rectangularPulse(-0.5, 0.5, t);
-%x=zeros(size(t)); x(1:fs/f:end)=1;                    
+%x = zeros(size(t)); x(1:fs/f:end)=1;                    
 %x = sin(2*pi*12*t) + sin(2*pi*5*t);
 %x = (t.^2 .* pi) + ((t-5).*t.^3);
-x = t;
-
-% Window function
-%x = x .* transpose(blackman(n));
-%x = x .* transpose(hann(n));
-%x = x .* transpose(hamming(n));
-%x = x .* transpose(rectwin(n));
+%x = t;
 
 plot(t,x);
 title('Base signal');
 grid on;
 
+% Window function
+x = x .* transpose(blackman(n));
+%x = x .* transpose(hann(n));
+%x = x .* transpose(hamming(n));
+%x = x .* transpose(rectwin(n));
+
 figure(2);
 y = fft(x);     
-f = (0:length(y)-1)*fs/length(y);                 
+%f = (0:length(y)-1)*fs/length(y);                 
 fshift = (-n/2:n/2-1)*(fs/n);
 yshift = fftshift(y);
 plot(fshift,abs(yshift)/(n/2));
