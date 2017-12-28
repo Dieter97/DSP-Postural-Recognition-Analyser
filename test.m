80bb113d980725b9d7ad41fde0cdee5d65a4b62f
Niels Balemans <balemans.niels@gmail.com>
2017-12-28 17:53:26 +0100

    update test

--- test.m
+++ test.m
@@ -1,24 +1,38 @@
 clear; clc;
 figure(1);
-t = 0:1/50:10-1/50;                     
-x = sin(2*pi*12*t) + sin(2*pi*5*t);
-n = length(x); 
+f=20; %Hz
+fs=f*10; 
+t=-1:1/fs:1;
+n = length(t);
+
+% function
+%x = rectangularPulse(-0.5, 0.5, t);
+%x=zeros(size(t)); x(1:fs/f:end)=1;                    
+%x = sin(2*pi*12*t) + sin(2*pi*5*t);
+%x = (t.^2 .* pi) + ((t-5).*t.^3);
+x = t;
+
+% Window function
 %x = x .* transpose(blackman(n));
+%x = x .* transpose(hann(n));
+%x = x .* transpose(hamming(n));
+%x = x .* transpose(rectwin(n));
+
 plot(t,x);
 title('Base signal');
 grid on;
 
 figure(2);
 y = fft(x);     
-f = (0:length(y)-1)*50/length(y);                 
-fshift = (-n/2:n/2-1)*(50/n);
+f = (0:length(y)-1)*fs/length(y);                 
+fshift = (-n/2:n/2-1)*(fs/n);
 yshift = fftshift(y);
 plot(fshift,abs(yshift)/(n/2));
 title('Magnitude');
 grid on;
 
 figure(3);
-xnoise = x + 2.5*gallery('normaldata',size(t),4);
+xnoise = x + 2.5*gallery('normaldata',size(t),6);
 %xnoise = xnoise .* transpose(blackman(n));
 ynoise = fft(xnoise);
 ynoiseshift = fftshift(ynoise);    
